8a4264e874d919c4397e26b3f6e9af8343581be9
crmartin <cmartindesign@gmail>
2017-05-01 15:26:04 -0400

    added custom css

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div className="text-center">
-      <h1>About Component</h1>
+      <h1 className="page-title">About</h1>
       <p>An exploration of <a href="https://facebook.github.io/react/">React</a> using <a href="http://openweathermap.org/">openweathermap</a>.</p>
     </div>
   )
